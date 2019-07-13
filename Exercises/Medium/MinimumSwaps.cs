c54b2077a6a4e14d32c5d87243f0dbf02fe8f434
walter.torricos <walticotc@gmail.com>
2019-07-13 08:58:25 -0400

    Add Medium exercise: MinimumSwaps.

---
+++ Exercises/Medium/MinimumSwaps.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Exercises.Medium
+{
+    /*
+     You are given an unordered array consisting of consecutive integers
+     from the set [1, 2, 3, ..., n] without any duplicates. You are allowed to swap
+     any two elements. You need to find the minimum number of swaps required to sort
+     the array in ascending order.
+
+     For example, given the array arr=[7,1,3,2,4,5,6] we perform the following steps:
+            [7,1,3,2,4,5,6]
+        1.  [1,7,3,2,4,5,6] swap(7,1)
+        2.  [1,2,3,7,4,5,6] swap(7,2)
+        3.  [1,2,3,4,7,5,6] swap(7,4)
+        4.  [1,2,3,4,5,7,6] swap(7,5)
+        5.  [1,2,3,4,5,6,7] swap(7,6)
+        It took 5 swaps to sort the array.
+
+     The function must return the miminum number of swaps to sort the array.
+     Constraints:
+        1 <= n <= 10^5
+        1 <= arr[i] <= n
+     */
+    public class MinimumSwaps
+    {
+        [Theory]
+        [InlineData(new int[] { 7, 1, 3, 2, 4, 5, 6 }, 5)]
+        [InlineData(new int[] { 4, 3, 1, 2 }, 3)]
+        public void Scenarios(IEnumerable<int> arr, int expected)
+        {
+            var actual = new MinimumSwapsSolution().Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Performance()
+        {
+            var arr = Enumerable.Range(1, 1000000);
+
+            var solveTask = Task.Run(() =>
+            {
+                var actual = new MinimumSwapsSolution().Solve(arr);
+                Assert.Equal(0, actual);
+            });
+
+            var completed = Task.WaitAny(new Task[] { solveTask }, millisecondsTimeout: 2000);
+            Assert.True(0 == completed, "The solution took too much to execute");
+        }
+    }
+}
---
+++ Exercises/Medium/MinimumSwapsSolution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises.Medium
+{
+    public class MinimumSwapsSolution
+    {
+        public int Solve(IEnumerable<int> input)
+        {
+            var arr = input.ToArray(); // for convenience we convert it to an array
+            // we iterate one time to associate value with position O(n)
+            var valuePosDictionary = new Dictionary<int, int>();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                valuePosDictionary.Add(arr[i], i);
+            }
+
+            var swaps = 0;
+            // we iterate again to swap positions O(n)
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                var current = arr[i];
+                var target = i + 1;
+                if (current == target) continue;
+                //swap arr
+                var targetPos = valuePosDictionary[target];
+                arr[i] = target;
+                arr[targetPos] = current;
+                //update dic
+                valuePosDictionary[target] = i;
+                valuePosDictionary[current] = targetPos;
+
+                swaps++;
+            }
+
+            return swaps;
+        }
+    }
+}
