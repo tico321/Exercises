cbcd437c759d3a468df356ac5d2cca1a2dcf5ad9
Walter Torricos <walter.torricos@avantica.net>
2019-07-15 14:55:28 -0400

    Add easy exercise: Hourglass 2D Array.

---
+++ Exercises/Easy/Hourglass2DArray.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+
+namespace Exercises.Easy
+{
+    /*
+
+        Given a 6*6 2D array, arr:
+            1 1 1 0 0 0
+            0 1 0 0 0 0
+            1 1 1 0 0 0
+            0 0 0 0 0 0
+            0 0 0 0 0 0
+            0 0 0 0 0 0
+
+        We define an hourglass in A to be a subset of values with indices falling in this pattern in arr's
+        graphical representation:
+            a b c
+              d
+            e f g
+
+        There are 16 hourglasses in arr, and an hourglass sum is the sum of an hourglass values. Calculate the hourglass
+        sum for every hourglass in arr, then print the maximum hourglass sum.
+
+        For example given the 2D array:
+            1 1 1 0 0 0
+            0 1 0 0 0 0
+            1 1 1 0 0 0
+            0 0 2 4 4 0
+            0 0 0 2 0 0
+            0 0 1 2 4 0
+
+        The output would be 19, because arr contains the following hourglasses:
+            1 1 1   1 1 0   1 0 0   0 0 0
+              1       0       0       0
+            1 1 1   1 1 0   1 0 0   0 0 0
+
+            0 1 0   1 0 0   0 0 0   0 0 0
+              1       1       0       0
+            0 0 2   0 2 4   2 4 4   4 4 0
+
+            1 1 1   1 1 0   1 0 0   0 0 0
+              0       2       4       4
+            0 0 0   0 0 2   0 2 0   2 0 0
+
+            0 0 2   0 2 4   2 4 4   4 4 0
+              0       0       2       0
+            0 0 1   0 1 2   1 2 4   2 4 0
+
+        The hourglass with the maximum sum is:
+            2 4 4
+              2
+            1 2 4
+     */
+    public class Hourglass2DArray
+    {
+        [Fact]
+        public void Scenario1()
+        {
+            var arr = new int[][]
+            {
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 1, 0, 0, 0, 0 },
+                new int[] { 1, 1, 1, 0, 0, 0 },
+                new int[] { 0, 0, 2, 4, 4, 0 },
+                new int[] { 0, 0, 0, 2, 0, 0 },
+                new int[] { 0, 0, 1, 2, 4, 0 }
+            };
+            var expected = 19;
+
+            var actual = new Hourglass2DArraySolution().Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Scenario2()
+        {
+            var arr = new int[][]
+            {
+                new int[] { 1, 1,  1,  0,  0, 0 },
+                new int[] { 0, 1,  0,  0,  0, 0 },
+                new int[] { 1, 1,  1,  0,  0, 0 },
+                new int[] { 0, 9,  2, -4, -4, 0 },
+                new int[] { 0, 0,  0, -2,  0, 0 },
+                new int[] { 0, 0, -1, -2, -4, 0 }
+            };
+            var expected = 13;
+
+            var actual = new Hourglass2DArraySolution().Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Exercises/Easy/Hourglass2DArraySolution.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercises.Easy
+{
+    public class Hourglass2DArraySolution
+    {
+        public int Solve(int[][] arr)
+        {
+            var hourglasses = new int[4,4];
+            var max = int.MinValue;
+            for (int i=0; i<4; i++)
+            {
+                for(int j=0; j<4; j++)
+                {
+                    hourglasses[i, j] = this.CalculateHourglass(arr, i, j);
+                    if(hourglasses[i, j] > max)
+                    {
+                        max = hourglasses[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        private int CalculateHourglass(int[][] arr, int i, int j)
+        {
+            return arr[i][j]     + arr[i][j + 1]     + arr[i][j + 2] +
+                                   arr[i + 1][j + 1] +
+                   arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+        }
+    }
+}
