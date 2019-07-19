e5f0e7324e071d2dbe5dbd090c7068e82db9d443
walter.torricos <walticotc@gmail.com>
2019-07-18 22:04:55 -0400

    Add easy problem: TheatreSquare

---
+++ Exercises/Easy/TheatreSquareSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercises.Easy
+{
+    public class TheatreSquareSolution
+    {
+        public int Solve(int n, int m, int a)
+        {
+            var horizontalFlagstones = 0;
+            do
+            {
+                n -= a;
+                horizontalFlagstones++;
+            } while (n > 0);
+
+            var verticalFlagstones = 0;
+            do
+            {
+                m -= a;
+                verticalFlagstones++;
+            } while (m > 0);
+
+            return horizontalFlagstones * verticalFlagstones;
+        }
+    }
+}
