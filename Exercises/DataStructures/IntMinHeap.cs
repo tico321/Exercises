d2c9d3b204ab346a084977140ca3f4a33f671953
walter.torricos <walticotc@gmail.com>
2019-07-31 23:53:05 -0400

    Refactor Priority Queue and add property tests for it.

---
+++ Exercises/DataStructures/IntMinHeap.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace Exercises.DataStructures
+{
+    /// <summary>
+    /// Min-heap (priority queue)
+    /// https://en.wikipedia.org/wiki/Heap_%28data_structure%29
+    /// https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp
+    /// </summary>
+    public class IntMinHeap
+    {
+        private readonly List<int> pairs = new List<int>();
+
+        public int Length { get => this.pairs.Count; }
+
+        public void Add(int val)
+        {
+            this.pairs.Add(val);
+            this.BubbleUp(this.pairs.Count - 1);
+        }
+
+        public int Peek()
+        {
+            return this.pairs[0];
+        }
+
+        public int Pop()
+        {
+            int result = this.pairs[0];
+
+            this.pairs[0] = this.pairs[this.pairs.Count - 1];
+            this.pairs.RemoveAt(this.pairs.Count - 1);
+
+            this.PushDown(0);
+
+            return result;
+        }
+
+        private int Parent(int n) => (n - 1) / 2;
+
+        private int Left(int n) => n * 2 + 1;
+
+        private int Right(int n) => n * 2 + 2;
+
+        private void BubbleUp(int idx)
+        {
+            var parentIdx = Parent(idx);
+            while (idx > 0 && this.pairs[parentIdx] > this.pairs[idx])
+            {
+                this.Swap(parentIdx, idx);
+                idx = parentIdx;
+                parentIdx = Parent(idx);
+            }
+        }
+
+        private void PushDown(int idx)
+        {
+            while (Left(idx) < this.pairs.Count)
+            {
+                int smallchildIdx = Left(idx);
+                if (Right(idx) < this.pairs.Count && this.pairs[Right(idx)] < this.pairs[smallchildIdx])
+                    smallchildIdx = Right(idx);
+
+                if (this.pairs[smallchildIdx] >= this.pairs[idx]) break;
+
+                this.Swap(smallchildIdx, idx);
+                idx = smallchildIdx;
+            }
+        }
+
+        private void Swap(int a, int b)
+        {
+            int temp = this.pairs[a];
+            this.pairs[a] = this.pairs[b];
+            this.pairs[b] = temp;
+        }
+    }
+}
---
+++ Exercises/DataStructures/IntMinHeapTest.cs
@@ -0,0 +1,77 @@
+﻿using FsCheck;
+using FsCheck.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises.DataStructures
+{
+    public class PriorityQueueTest
+    {
+        [Property]
+        public Property TheElementWithMostPriorityIsAlwaysPopedFirst(int[] arr)
+        {
+            var sorted = arr.OrderBy(i => i).ToList();
+            var sut = new IntMinHeap();
+
+            foreach (var i in arr) sut.Add(i);
+            var actual = new int[arr.Length];
+            for (var i = 0; i < arr.Length; i++) actual[i] = sut.Pop();
+
+            return (sorted.SequenceEqual(actual)).ToProperty();
+        }
+
+        [Property]
+        public Property RandomOperations(PriorityQueueTestOperations[] operations, int[] arr)
+        {
+            var oracle = new List<int>();
+            var sut = new IntMinHeap();
+
+            var i = -1;
+            foreach (var op in operations)
+            {
+                if (op == PriorityQueueTestOperations.Add)
+                {
+                    i++;
+                    if (arr.Length <= i) continue;
+                    oracle.Add(arr[i]);
+                    oracle = oracle.OrderBy(e => e).ToList();
+                    sut.Add(arr[i]);
+                    if (oracle.Count != sut.Length) return (oracle.Count == sut.Length).ToProperty();
+                }
+                if (op == PriorityQueueTestOperations.Length)
+                {
+                    if (oracle.Count != sut.Length) return (oracle.Count == sut.Length).ToProperty();
+                }
+                if (op == PriorityQueueTestOperations.Pop)
+                {
+                    if (!oracle.Any()) continue;
+                    var expected = oracle.ElementAt(0);
+                    oracle.RemoveAt(0);
+                    var actual = sut.Pop();
+
+                    if (expected != actual) return (expected == actual).ToProperty();
+                }
+                if (op == PriorityQueueTestOperations.Peek)
+                {
+                    if (!oracle.Any()) continue;
+                    var expected = oracle.ElementAt(0);
+                    var actual = sut.Peek();
+
+                    if (expected != actual) return (expected == actual).ToProperty();
+                    if (oracle.Count != sut.Length) return (oracle.Count == sut.Length).ToProperty();
+                }
+            }
+
+            return (true == true).ToProperty();
+        }
+
+        public enum PriorityQueueTestOperations
+        {
+            Add,
+            Pop,
+            Peek,
+            Length
+        }
+    }
+}
