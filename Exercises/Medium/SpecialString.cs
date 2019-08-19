cf0ab5693acf2ef22f03b96d5007472056fbbcbf
Walter Torricos <walter.torricos@avantica.net>
2019-08-19 19:36:04 -0400

    Add medium exercise Special String.

---
+++ Exercises/Medium/SpecialString.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Exercises.Medium
+{
+    /*
+        A string is said to be a special string if either of two conditions is met:
+            * All of the characters are the same, e.g. aaa.
+            * All characters except the middle one are the same, e.g. aadaa.
+        A special substring is any substring of a string which meets one of those criteria. Given a string,
+        determine how many special substrings can be formed from it.
+
+        For example, given the string s=mnonopoo, we have the following special substrings:
+        {m,n,o,n,o,p,o,o,non,ono,opo,oo}
+        Constraints
+            * 1 <= n <= 10^6 where n is the size of the string
+            * s[i] is inside the set [a-z]
+     */
+    public class SpecialString
+    {
+        [Theory]
+        [InlineData(8, "mnonopoo", 12)]
+        [InlineData(5, "asasd", 7)] // [a, s, a, s, d, asa, sas]
+        [InlineData(7, "abcbaba", 10)] // [a, b, c, b, a, b, a, bcb, bab, aba]
+        [InlineData(4, "aaaa", 10)] //[a, a, a, a, aa, aa, aa, aaa, aaa, aaa]
+        public void Scenarios(int n, string s, int expected)
+        {
+            var actual = SpecialStringSolution.Solve(n, s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Exercises/Medium/SpecialStringSolution.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Exercises.Medium
+{
+    public static class SpecialStringSolution
+    {
+        public static long Solve(int n, string s)
+        {
+            var count = s.Length; //count every char
+
+            //count the ones with the middle one different
+            var exceptMiddleOne = new Regex("(([a-z])\\2*)(?!\\1)(?=[a-z]\\1)"); //matches a_a and also aa_aa
+            count += exceptMiddleOne.Matches(s).Select(m => m.Groups[0].Length).Sum();
+
+            //count repeated chars
+            var repeated = new Regex("([a-z])\\1+");
+            count += repeated.Matches(s)
+                .Select(m => m.Groups[0].Length - 1)
+                .Select(l =>
+                    (l * l + l) /
+                    2) // triangular number given by (n^2+n)/2 // https://en.wikipedia.org/wiki/Triangular_number
+                .Sum();
+
+            return count;
+        }
+    }
+}
