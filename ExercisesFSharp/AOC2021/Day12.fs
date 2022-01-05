107e3450c6293cbe9e744e278c08edea230e186e
Walter Torricos <ticot55@gmail.com>
2022-01-04 23:22:20 -0400

    AOC day12 solution 2 and refactor solution 1 inspired on bread first search but solution 2 turned out a bit slow

--- ExercisesFSharp/AOC2021/Day12.fs
+++ ExercisesFSharp/AOC2021/Day12.fs
@@ -4,57 +4,71 @@ open FSharpPlus
 open Xunit
 open System
 
-let findPaths input =
+let isUpper (str: string) = str.ToCharArray() |> Array.forall Char.IsUpper
+
+let toGraph input =
     let addToGraph (o, d) (routeMap: Map<string, string list>) =
         if not <| routeMap.ContainsKey(o) then routeMap.Add(o, [d])
         else routeMap.Change(o, (Option.bind (fun ds -> (d::ds |> List.distinct) |> Some)))
 
-    let isUpper (str: string) = str.ToCharArray() |> Array.forall Char.IsUpper
+    input
+    |> List.map (sscanf "%s-%s")
+    |> List.fold (fun (m: Map<string, string list>) (o, d) -> m |> addToGraph (o,d) |> addToGraph (d, o)) Map.empty
 
-    let graph =
-        input
-        |> List.map (sscanf "%s-%s")
-        |> List.fold
-            (fun (routeMap: Map<string, string list>) (o, d) -> routeMap |> addToGraph (o,d) |> addToGraph (d, o))
-            Map.empty
+let rec explorePaths paths goalTest getSuccessors =
+    match goalTest paths, getSuccessors paths with
+    | _, None -> paths
+    | true, _ -> paths
+    | false, Some successors -> explorePaths successors goalTest getSuccessors
 
-    let rec explorePaths (paths: string list list) (visited: string Set) : string list list =
-        paths
-        |> List.collect
-               (function
+let goalTest paths = paths |> List.forall (function x::_ -> x = "end" | _ -> false)
+
+let getSuccessors isValid (graph: Map<string, string list>) paths =
+        let successors =
+            paths
+            |> List.collect (function
                 | x::xs when x = "end" -> [x::xs]
-                | x::xs ->
-                    match graph.TryGetValue(x) with
-                    | false, _ -> []
-                    | true, destinations ->
-                        destinations
-                        |> List.filter (fun p -> not <| visited.Contains(p))
-                        |> List.collect (fun next ->
-                            let nextVisited = if next |> isUpper then visited else visited.Add(next)
-                            explorePaths [next::x::xs] nextVisited)
-                | xs -> [xs])
+                | x::xs when graph.ContainsKey(x) ->
+                    graph[x]
+                    |> List.map (fun next -> next::x::xs)
+                    |> List.filter isValid
+                    |> List.distinct
+                | _ -> [])
+        match successors with
+        | [] -> None
+        | s -> Some s
 
-    let graphToStringPath paths =
-        paths
-        |> List.map (fun arr -> String.Join(",", arr |> List.rev))
-        |> List.sortByDescending (fun p -> p.Length)
+let solution1 input =
+    let graph = input |> toGraph
+    let isValid path =
+        path
+        |> List.groupBy id
+        |> List.forall (fun (node, nodeOccurrences) -> node |> isUpper || nodeOccurrences |> length = 1)
 
-    explorePaths [["start"]] (Set.empty.Add("start")) |> graphToStringPath
+    let getSuccessors' = getSuccessors isValid graph
 
-[<Theory>]
-[<InlineData("start-A;A-end", "start,A,end")>]
-[<InlineData("start-A;A-b;b-end", "start,A,b,end")>]
-[<InlineData("A-b;b-end;start-A", "start,A,b,end")>]
-[<InlineData("start-a;start-b;a-b;b-end", "start,a,b,end;start,b,end")>]
-[<InlineData("start-a;start-b;a-b;b-end;a-end", "start,b,a,end;start,a,b,end;start,b,end;start,a,end")>]
-[<InlineData("start-A;A-c;A-end", "start,A,c,A,end;start,A,end")>]
-let ``day12 solution 1 samples`` (input: string) (expected: string) =
-    let parsedInput = input.Split(";") |> Array.toList
-    let parsedExpected = expected.Split(";") |> Array.toList
+    explorePaths [["start"]] goalTest getSuccessors'
+    |> length
 
-    let actual = findPaths parsedInput
+let solution2 input =
+    let graph = input |> toGraph
+    let isValid paths =
+        let nodeVisitCount = paths |> List.groupBy id |> List.map (fun (k, list) -> k, length list)
+        let startCount = nodeVisitCount |> List.find (fun (n, _) -> n = "start") |> snd
 
-    Assert.Equal<string list>(parsedExpected, actual)
+        if (startCount <> 1) then
+            false
+        else
+            let smallCaves = nodeVisitCount |> List.filter (fun (n, _) -> not <| isUpper n)
+            let smallCavesVisitedTwice = smallCaves |> List.filter (fun (_, ns) -> ns = 2)
+            let smallCavesVisitedMoreThanTwice = smallCaves |> List.filter (fun (_, ns) -> ns > 2)
+
+            (smallCavesVisitedTwice.Length <= 1) && (List.isEmpty smallCavesVisitedMoreThanTwice)
+
+    let getSuccessors' = getSuccessors isValid graph
+
+    explorePaths [["start"]] goalTest getSuccessors'
+    |> length
 
 [<Theory>]
 [<InlineData("day12_sample1.txt", 10)>]
@@ -63,6 +77,17 @@ let ``day12 solution 1 samples`` (input: string) (expected: string) =
 let ``day12 solution 1`` file expected =
     let input = InputUtils.readFile file |> Seq.toList
 
-    let actual = findPaths input |> List.length
+    let actual = solution1 input
+
+    Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData("day12_sample1.txt", 36)>]
+[<InlineData("day12_sample2.txt", 103)>]
+// [<InlineData("day12_input.txt", 144309)>] //slow solution
+let ``day12 solution 2`` file expected =
+    let input = InputUtils.readFile file |> Seq.toList
+
+    let actual = solution2 input
 
     Assert.Equal(expected, actual)
